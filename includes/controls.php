b7f8c0141a2aa3f6c94bc7812809a08d0be309e3
Rezwanul Haque <74784389+REZWANUL1@users.noreply.github.com>
2023-09-04 20:12:12 +0600

    adding country selected field

--- includes/controls.php
+++ includes/controls.php
@@ -12,43 +12,57 @@ function pqc_display_qr_code_settings()
    add_settings_field(
       'pqc_height',
       __('Qr code Height', 'pqc_settings_init'),
-      'pqc_display_height',
+      'pqc_display_field',
       'general',
-      'pqc_section'
+      'pqc_section',
+      array('pqc_height')
    );
    add_settings_field(
       'pqc_width',
       __('Qr code Width', 'pqc_settings_init'),
-      'pqc_display_width',
+      'pqc_display_field',
+      'general',
+      'pqc_section',
+      array('pqc_width')
+   );
+   add_settings_field(
+      'pqc_country', 
+      __('Select Country', 'pqc_settings_init'),
+      'pqc_display_select_field', 
       'general',
       'pqc_section'
    );
    register_setting('general', 'pqc_height', array('sanitize_callback' => 'esc_attr'));
    register_setting('general', 'pqc_width', array('sanitize_callback' => 'esc_attr'));
+   register_setting('general', 'pqc_country', array('sanitize_callback' => 'esc_attr'));
 }
+
+function pqc_display_select_field()
+{
+   $option = get_option('pqc_country'); 
+   $countries = array('None', 'Afghanistan', 'Bangladesh', 'India', 'Nepal');
+
+   printf('<select id="%s" name="%s"  >', 'pqc_country', 'pqc_country'); 
+   foreach ($countries as $country) {
+      $selected = ($option == $country) ? 'selected' : '';
+      printf('<option value="%s" %s>%s</option>', $country, $selected, $country);
+   }
+   echo '</select>';
+}
+
 function    pqc_section_callback()
 {
    $settings_heading = 'Settings for posts to QR plugin';
    echo '<p>' . esc_html__($settings_heading, 'pqc-by-rez') . '</p>';
 }
 
-function pqc_display_height()
+function pqc_display_field($args)
 {
-   $height = get_option('pqc_height');
+   $option = get_option($args[0]);
 ?>
    <?php
-   printf('<input type="text" id="%s" name="%s" value="%s" />', 'pqc_height', 'pqc_height', $height);   ?>
+   printf('<input type="text" id="%s" name="%s" value="%s" />', $args[0], $args[0], $option);   ?>
 
    <p class="description"><?php _e('Enter the QR code height in pixels.', 'pqc_settings_init'); ?></p>
-<?php
-}
-function pqc_display_width()
-{
-   $width = get_option('pqc_width');
-?>
-   <?php
-   printf('<input type="text" id="%s" name="%s" value="%s" />', 'pqc_width', 'pqc_width', $width);   ?>
-
-   <p class="description"><?php _e('Enter the QR code width in pixels.', 'pqc_settings_init'); ?></p>
 <?php
 }
